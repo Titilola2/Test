fb767d9354c3fd13f2712f9933899607a2ddce07
Grace Olafare <oluwafunhmi@gmail.com>
2024-04-11 10:33:12 +0000

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,264 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      <div class="w-96 h-96 relative bg-stone-50">
+  <div class="w-96 h-96 left-0 top-[3217px] absolute">
+    <div class="w-96 h-96 left-0 top-[171.35px] absolute bg-stone-400"></div>
+    <div class="w-96 h-96 left-[50px] top-[71px] absolute">
+      <img class="w-96 h-72 left-0 top-0 absolute" src="https://via.placeholder.com/427x301" />
+      <div class="w-96 h-56 left-[228px] top-[200.70px] absolute">
+        <div class="w-96 h-56 left-0 top-0 absolute bg-stone-600"></div>
+        <div class="w-96 h-36 left-[27px] top-[41.30px] absolute">
+          <div class="w-96 h-10 left-0 top-0 absolute text-stone-50 text-3xl font-black font-['Inter'] uppercase tracking-widest">Contact Us Now</div>
+          <div class="w-48 h-6 left-[11px] top-[67px] absolute justify-start items-center gap-5 inline-flex">
+            <div class="w-6 h-6 flex-col justify-center items-center inline-flex">
+              <div class="text-stone-50 text-2xl font-black font-['Font Awesome 5 Free']"></div>
+            </div>
+            <div class="text-stone-50 text-base font-semibold font-['Inter']">+1 (240) 818-5628</div>
+          </div>
+          <div class="w-64 h-6 left-[11px] top-[113px] absolute justify-start items-center gap-5 inline-flex">
+            <div class="w-6 h-6 px-0.5 py-1 justify-center items-center flex">
+              <div class="w-5 h-4 relative">
+              </div>
+            </div>
+            <div class="text-stone-50 text-base font-semibold font-['Inter']">care@doobcommunity.com</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-96 left-[735px] top-[264px] absolute text-white text-6xl font-black font-['Inter'] uppercase tracking-widest">Your Certified Quality Service Providers</div>
+  </div>
+  <div class="h-96 px-24 py-9 left-0 top-[2224px] absolute justify-center items-center inline-flex">
+    <div class="w-96 h-96 relative flex-col justify-start items-start flex">
+      <div class="flex-col justify-start items-center gap-7 inline-flex">
+        <div class="text-stone-950 text-6xl font-semibold font-['Inter']">What We Do</div>
+        <div class="w-96 text-center text-stone-950 text-xl font-medium font-['Inter']">DOOB Community offers an extensive array of meticulously tailored programs and services designed to address the distinct needs of each individual. Our professional offerings include Meaningful Day Services, Residential Services, Support Services, and specialized support for Transitioning Youth.</div>
+      </div>
+      <div class="px-8 py-2 justify-center items-center inline-flex">
+        <div class="self-stretch flex-col justify-start items-center gap-5 inline-flex">
+          <div class="w-16 h-16 relative">
+            <div class="w-16 h-16 left-0 top-0 absolute bg-green-100 rounded-full"></div>
+            <div class="w-16 h-16 left-0 top-0 absolute bg-stone-600 rounded-md"></div>
+            <div class="w-6 h-6 left-[20px] top-[20px] absolute"></div>
+          </div>
+          <div class="w-72 text-center text-slate-800 text-2xl font-bold font-['Inter'] leading-loose">Residential Services</div>
+          <div class="w-72 text-center text-stone-950 text-base font-medium font-['Inter'] leading-normal">Experience supportive living with our Residential Services. We provide a safe and empowering environment, fostering independence and a sense of belonging for each resident.</div>
+        </div>
+      </div>
+      <div class="py-5 justify-center items-center inline-flex">
+        <div class="self-stretch flex-col justify-start items-center gap-5 inline-flex">
+          <div class="w-16 h-16 relative">
+            <div class="w-16 h-16 left-0 top-0 absolute bg-stone-600 rounded-md"></div>
+            <div class="w-6 h-6 left-[20px] top-[20px] absolute"></div>
+          </div>
+          <div class="w-96 text-center text-slate-800 text-2xl font-bold font-['Inter'] leading-loose">Meaningful Day Services</div>
+          <div class="w-72 text-center text-stone-950 text-base font-medium font-['Inter'] leading-normal">Enrich your days with our Meaningful Day Services—tailored programs fostering community, personal growth, and fulfillment.</div>
+        </div>
+      </div>
+      <img class="w-96 h-96 rounded-3xl" src="https://via.placeholder.com/446x634" />
+      <div class="justify-center items-center inline-flex">
+        <div class="self-stretch flex-col justify-start items-center gap-5 inline-flex">
+          <div class="w-16 h-16 relative">
+            <div class="w-16 h-16 left-0 top-0 absolute bg-stone-600 rounded-md"></div>
+            <div class="w-6 h-6 left-[20px] top-[20px] absolute"></div>
+          </div>
+          <div class="w-96 text-center text-slate-800 text-2xl font-bold font-['Inter'] leading-loose">Support Services</div>
+          <div class="w-72 text-center text-stone-950 text-base font-medium font-['Inter'] leading-normal">Discover the support you need with our comprehensive Support Services. From healthcare to skill development, we're committed to addressing diverse needs and enhancing your well-being.</div>
+        </div>
+      </div>
+      <div class="px-8 justify-center items-center inline-flex">
+        <div class="self-stretch flex-col justify-start items-center gap-5 inline-flex">
+          <div class="w-16 h-16 relative">
+            <div class="w-16 h-16 left-0 top-0 absolute bg-stone-600 rounded-md"></div>
+            <div class="w-6 h-6 left-[20px] top-[20px] absolute"></div>
+          </div>
+          <div class="w-72 text-center text-slate-800 text-2xl font-bold font-['Inter'] leading-loose">Transitioning Youth.</div>
+          <div class="w-72 text-center text-stone-950 text-base font-medium font-['Inter'] leading-normal">Navigate the journey to adulthood seamlessly with our Transitioning Youth support. We equip young individuals with the skills and resources needed for a confident transition into independence.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-96 h-96 left-0 top-[931px] absolute">
+    <div class="h-14 p-2 left-[243px] top-[-239px] absolute bg-stone-600 rounded-lg justify-center items-center gap-2 inline-flex">
+      <div class="text-center text-white text-xl font-semibold font-['Inter']">Contact Us</div>
+    </div>
+    <div class="left-[347px] top-[24px] absolute flex-col justify-start items-center gap-7 inline-flex">
+      <div class="text-stone-950 text-6xl font-semibold font-['Inter']">Our Mission</div>
+      <div class="flex-col justify-start items-center gap-6 flex">
+        <div class="flex-col justify-start items-center gap-4 flex">
+          <div class="text-center text-stone-950 text-3xl font-semibold font-['Inter']">Empowering Connections for Every Ability.</div>
+          <div class="w-96 text-center text-stone-950 text-xl font-normal font-['Inter']">Our mission is dedicated to fostering empowerment among individuals with disabilities, encouraging active engagement, and ultimately contributing to an enriched quality of life</div>
+        </div>
+        <div class="h-14 p-2 bg-stone-600 rounded-lg justify-center items-center gap-2 inline-flex">
+          <div class="text-center text-white text-xl font-semibold font-['Inter']">Learn More</div>
+        </div>
+      </div>
+    </div>
+    <div class="left-[86px] top-[388px] absolute justify-start items-center gap-14 inline-flex">
+      <div class="w-96 h-96 relative">
+        <img class="w-96 h-96 left-[114px] top-0 absolute" src="https://via.placeholder.com/541x359" />
+        <img class="w-96 h-96 left-0 top-[240.79px] absolute" src="https://via.placeholder.com/541x359" />
+        <img class="w-96 h-96 left-[114px] top-[514.37px] absolute" src="https://via.placeholder.com/541x359" />
+      </div>
+      <div class="flex-col justify-start items-start gap-12 inline-flex">
+        <div class="w-96 h-48 pr-2.5 bg-stone-600 rounded-md justify-start items-center gap-3 inline-flex">
+          <img class="w-56 h-48 relative" src="https://via.placeholder.com/220x188" />
+          <div class="flex-col justify-start items-center gap-2 inline-flex">
+            <div class="text-white text-3xl font-semibold font-['Inter']">Explore our Services</div>
+            <div class="w-80 text-center text-stone-50 text-base font-normal font-['Inter']">Embark on a journey of discovery with "Explore Our Services," where tailored programs and comprehensive support await, empowering individuals to thrive and achieve their unique goals.</div>
+          </div>
+        </div>
+        <div class="w-96 h-48 pr-2.5 bg-stone-600 rounded-md justify-start items-center gap-3 inline-flex">
+          <img class="w-56 h-48 relative" src="https://via.placeholder.com/220x188" />
+          <div class="flex-col justify-start items-center gap-2 inline-flex">
+            <div class="text-white text-3xl font-semibold font-['Inter']">Join Our Team</div>
+            <div class="w-80 text-center text-stone-50 text-base font-normal font-['Inter']">Become a valued member of our team! Explore exciting opportunities and contribute your skills to a dynamic and supportive work environment. Join us in making a positive impact and shaping a brighter future together.</div>
+          </div>
+        </div>
+        <div class="w-96 h-48 pr-2.5 bg-stone-600 rounded-md justify-start items-center gap-3 inline-flex">
+          <img class="w-56 h-48 relative" src="https://via.placeholder.com/220x188" />
+          <div class="flex-col justify-start items-center gap-2 inline-flex">
+            <div class="text-white text-3xl font-semibold font-['Inter']">Support Our Mission</div>
+            <div class="w-80 text-center text-stone-50 text-base font-normal font-['Inter']">oin our mission to empower lives through support. Your contribution fosters independence and enhances quality of life for individuals facing disabilities. Together, let's make a positive impact.</div>
+          </div>
+        </div>
+        <div class="w-96 h-48 pr-2.5 bg-stone-600 rounded-md justify-start items-center gap-3 inline-flex">
+          <img class="w-56 h-48 relative" src="https://via.placeholder.com/220x188" />
+          <div class="flex-col justify-start items-center gap-2 inline-flex">
+            <div class="text-white text-3xl font-semibold font-['Inter']">Follow Our Events</div>
+            <div class="w-80 text-center text-stone-50 text-base font-normal font-['Inter']">Stay connected and follow our events for exciting updates, impactful initiatives, and opportunities to engage with our community. Don't miss out on the latest happenings – be a part of our journey!</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-96 h-96 left-0 top-[160px] absolute">
+    <div class="w-96 h-96 left-0 top-0 absolute bg-gradient-to-r from-zinc-800 to-zinc-300"></div>
+    <div class="left-[90px] top-[183px] absolute flex-col justify-start items-start gap-8 inline-flex">
+      <div class="w-96 text-white text-6xl font-semibold font-['Inter']">Empowering Connections for Every Ability.</div>
+      <div class="w-96 text-white text-2xl font-normal font-['Inter']">empowers adults with intellectual and developmental disabilities, fostering independence and enhancing quality of life through comprehensive support services.<br/>they don't have disability but an ability</div>
+      <div class="justify-start items-start gap-7 inline-flex">
+        <div class="w-32 p-2 bg-stone-600 rounded-lg justify-center items-center gap-2 flex">
+          <div class="text-center text-white text-xl font-semibold font-['Inter']">Contact Us</div>
+        </div>
+        <div class="w-32 p-2 rounded-lg border border-stone-600 justify-center items-center gap-2 flex">
+          <div class="w-28 text-center text-white text-xl font-semibold font-['Inter']">Enroll</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-96 h-24 px-24 py-1.5 left-0 top-[62px] absolute bg-stone-50 justify-center items-center inline-flex">
+    <div class="h-20 justify-between items-center inline-flex">
+      <div class="flex-col justify-center items-center inline-flex">
+        <div class="w-16 h-16 relative">
+        </div>
+        <div class="text-center text-stone-600 text-sm font-black font-['Playfair Display']">DOOB COMMUNITY</div>
+      </div>
+      <div class="justify-start items-center gap-12 flex">
+        <div class="justify-start items-center gap-5 flex">
+          <div class="text-center text-black text-base font-semibold font-['Inter']">About Us</div>
+          <div class="text-center text-black text-base font-semibold font-['Inter']">People</div>
+          <div class="text-center text-black text-base font-semibold font-['Inter']">Services</div>
+        </div>
+        <div class="justify-start items-start gap-2 flex">
+          <div class="w-32 p-2 bg-stone-600 rounded-lg justify-center items-center gap-2 flex">
+            <div class="w-28 text-center text-white text-xl font-semibold font-['Inter']">Donate</div>
+          </div>
+          <div class="w-32 p-2 bg-stone-600 rounded-lg justify-center items-center gap-2 flex">
+            <div class="text-center text-white text-xl font-semibold font-['Inter']">Contact Us</div>
+          </div>
+          <div class="w-32 p-2 rounded-lg border border-stone-600 justify-center items-center gap-2 flex">
+            <div class="w-28 text-center text-stone-600 text-xl font-semibold font-['Inter']">Enroll</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-96 h-96 left-0 top-[3875px] absolute bg-stone-50">
+    <div class="w-96 h-12 left-0 top-[384px] absolute">
+      <div class="w-96 h-14 left-[1440px] top-0 absolute origin-top-left rotate-180 bg-stone-600 rounded-tr-full"></div>
+      <div class="w-96 h-14 left-0 top-0 absolute bg-stone-600 rounded-tr-full"></div>
+      <div class="left-[634px] top-[4px] absolute flex-col justify-start items-center inline-flex">
+        <div class="w-36 h-6 relative">
+          <div class="w-6 h-6 left-[118px] top-0 absolute">
+            <div class="w-6 h-6 left-0 top-0 absolute"></div>
+            <div class="w-4 h-4 left-[4px] top-[3px] absolute"></div>
+          </div>
+          <div class="w-6 h-6 left-[79px] top-0 absolute">
+            <div class="w-6 h-6 left-0 top-0 absolute"></div>
+            <div class="w-4 h-3.5 left-[4px] top-[4px] absolute"></div>
+          </div>
+          <div class="w-6 h-6 left-[39px] top-0 absolute">
+            <div class="w-6 h-6 left-0 top-0 absolute"></div>
+            <div class="w-4 h-3.5 left-[3px] top-[5px] absolute"></div>
+          </div>
+          <div class="w-6 h-6 left-0 top-0 absolute">
+            <div class="w-6 h-6 left-0 top-0 absolute"></div>
+            <div class="w-1.5 h-3.5 left-[8px] top-[5px] absolute"></div>
+          </div>
+        </div>
+        <div class="text-center"><span style="text-stone-600 text-xs font-normal font-['Inter'] leading-normal">@2024 DOOB Communiy </span><span style="text-stone-600 text-xs font-normal font-['Inter'] capitalize leading-normal">All Rights Reserved</span></div>
+      </div>
+    </div>
+    <div class="w-96 h-72 left-[204px] top-[41.04px] absolute">
+      <div class="left-[438px] top-0 absolute flex-col justify-center items-center inline-flex">
+        <div class="w-16 h-16 relative">
+        </div>
+        <div class="text-center text-stone-600 text-base font-black font-['Playfair Display']">DOOB COMMUNITY</div>
+      </div>
+      <div class="left-[223px] top-[129.20px] absolute justify-start items-center gap-10 inline-flex">
+        <div class="justify-start items-start gap-6 flex">
+          <div class="text-center text-stone-600 text-lg font-semibold font-['Inter']">About Us</div>
+          <div class="w-7 h-px origin-top-left rotate-90 border-2 border-stone-600"></div>
+        </div>
+        <div class="justify-start items-start gap-6 flex">
+          <div class="text-center text-stone-600 text-lg font-semibold font-['Inter']">People</div>
+          <div class="w-7 h-px origin-top-left rotate-90 border-2 border-stone-600"></div>
+        </div>
+        <div class="justify-start items-start gap-6 flex">
+          <div class="text-center text-stone-600 text-lg font-semibold font-['Inter']">News </div>
+          <div class="w-7 h-px origin-top-left rotate-90 border-2 border-stone-600"></div>
+        </div>
+        <div class="justify-start items-start gap-6 flex">
+          <div class="text-center text-stone-600 text-lg font-semibold font-['Inter']">Services</div>
+          <div class="w-7 h-px origin-top-left rotate-90 border-2 border-stone-600"></div>
+        </div>
+        <div class="p-2 justify-center items-center gap-2 flex">
+          <div class="text-center text-stone-600 text-lg font-semibold font-['Inter']">Blogs</div>
+        </div>
+      </div>
+      <div class="w-96 left-0 top-[206.96px] absolute text-center text-stone-600 text-sm font-normal font-['Inter'] leading-normal">DOOB Community is committed to nondiscriminatory practices in all aspects of its services and programs. This commitment aligns with regulations from acts such as the 1964 Civil Rights Act, the Rehabilitation Act of 1973, the Age Discrimination Act of 1975, and the Hill Burton Act. We ensure equal opportunities in employment, admissions, service provisions, accommodations, and program participation, rejecting discrimination based on factors like race, color, national origin, age, and disability.</div>
+    </div>
+  </div>
+  <div class="w-96 px-24 py-4 left-0 top-0 absolute bg-stone-600 justify-center items-center inline-flex">
+    <div class="grow shrink basis-0 self-stretch justify-between items-center inline-flex">
+      <div class="justify-start items-start gap-2 flex">
+        <div class="w-6 h-6 flex-col justify-center items-center inline-flex">
+          <div class="text-white text-2xl font-black font-['Font Awesome 5 Free']"></div>
+        </div>
+        <div class="text-white text-base font-semibold font-['Inter']">+1 (240) 818-5628</div>
+      </div>
+      <div class="justify-start items-start gap-2 flex">
+        <div class="w-6 h-6 px-0.5 py-1 justify-center items-center flex">
+          <div class="w-5 h-4 relative">
+          </div>
+        </div>
+        <div><span style="text-white text-base font-semibold font-['Inter']">c</span><span style="text-white text-base font-semibold font-['Inter'] underline">are@doobcommunity.com</span></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
