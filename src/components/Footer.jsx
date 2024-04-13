2d67f2c2f7370a69add3a16ae12ad0329be9bdbc
Grace Olafare <oluwafunhmi@gmail.com>
2024-04-13 13:38:25 +0100

    secondcommit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,28 @@ const Footer = () => {
   
     
 
-<div className="Frame23 w-full h-96 relative">
-  <div className="Background w-full lg:h-full h-2/3 left-0  absolute bg-lime-600" />
+<div className="Frame23 w-full h-96 mt-40 flex flex-row  relative">
+  <div className="Background w-full lg:h-full h-2/5 left-0  absolute bg-lime-600" />
+     <div className="RightColumn md:w-1/4 w-1/3 h-1/3 left-50 md:px-8 px-2 top-[-20px] md:top-[-50px] absolute">
+     <img className="ImagePlaceholder w-full ml-4 " src="https://via.placeholder.com/427x301" />
+     </div>
    
+     <div className="w-2/5 md:w-2/5 md:ml-30 md:left-40 ml-10 md:top-20 top-10 left-5  absolute">
+      <div className="Rectangle14 w-4/5  md:min-h-56 h-20  absolute bg-lime-950" />
+      <div className="ContentIconBox flex flex-col  px-2 md:px-4 pt-4  absolute">
+
+      <div className="ContactUsNow w-full text-stone-50 text-xs md:text-4xl font-black font-['Inter'] uppercase ">Contact Us Now</div>
+      <div className="">
+        <div className="call-Icon ">  I      </div>
+        <div className=""> </div>
+      </div>
+        
+
+      </div>
+    </div>
+    
   
-  <div className="YourCertifiedQualityServiceProviders w-3/5 right-1 top-10 absolute text-white lg:text-6xl md:text-4xl text-xl font-black font-['Inter'] uppercase tracking-widest">Your Certified Quality Service Providers</div>
+  <div className="YourCertifiedQualityServiceProviders w-1/2 -right-4 md:rigth-0 flex-glow  md:my-20 my-12 absolute text-white lg:text-6xl md:text-3xl-flex text-sm font-bold font-['BoldInter'] uppercase tracking-widest">Your Certified Quality Service Providers</div>
 </div>
 
 
@@ -18,3 +35,35 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
+
+<div className="Frame23 w-96 h-96 relative">
+  <div className="Background w-96 h-96 left-0 top-[171.35px] absolute bg-stone-400" />
+  <div className="RightColumn w-96 h-96 left-[50px] top-[71px] absolute">
+    <img className="ImagePlaceholder w-96 h-72 left-0 top-0 absolute" src="https://via.placeholder.com/427x301" />
+    <div className="IconBox w-96 h-56 left-[228px] top-[200.70px] absolute">
+      <div className="Rectangle14 w-96 h-56 left-0 top-0 absolute bg-stone-600" />
+      <div className="ContentIconBox w-96 h-36 left-[27px] top-[41.30px] absolute">
+
+
+        <div className="ContactUsNow w-96 h-10 left-0 top-0 absolute text-stone-50 text-3xl font-black font-['Inter'] uppercase tracking-widest">Contact Us Now</div>
+        <div className="Frame32 w-48 h-6 left-[11px] top-[67px] absolute justify-start items-center gap-5 inline-flex">
+          <div className=" w-6 h-6 flex-col justify-center items-center inline-flex">
+            <div className=" text-stone-50 text-2xl font-black font-['Font Awesome 5 Free']"></div>
+          </div>
+          <div className="12408185628 text-stone-50 text-base font-semibold font-['Inter']">+1 (240) 818-5628</div>
+        </div>
+        <div className="Frame33 w-64 h-6 left-[11px] top-[113px] absolute justify-start items-center gap-5 inline-flex">
+          <div className="IconamoonEmailLight w-6 h-6 px-0.5 py-1 justify-center items-center flex">
+            <div className="Group w-5 h-4 relative">
+            </div>
+          </div>
+          <div className="CareDoobcommunityCom text-stone-50 text-base font-semibold font-['Inter']">care@doobcommunity.com</div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <div className="YourCertifiedQualityServiceProviders w-96 left-[735px] top-[264px] absolute text-white text-6xl font-black font-['Inter'] uppercase tracking-widest">Your Certified Quality Service Providers</div>
+</div>
